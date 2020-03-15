6e7ad430753c51d34a01e2ff16f4491450725ac3
tamdee <tamdee12@gmailcom>
2020-03-15 16:13:52 +0000

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,47 @@
 console.log("Up and running!");
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "imaged/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds"
+}
+];
 const cardsInPlay = [];
 
 function checkForMatch() {
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 }
-}
+};
 
 function flipCard(cardId) {
 
-	alert("User flipped " + cards[cardId]);
-	cardsInPlay.push([cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push([cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 
