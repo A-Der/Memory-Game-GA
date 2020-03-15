fdba658b0700f08472465e220061ee3c4c42e40e
tamdee <tamdee12@gmailcom>
2020-03-15 19:37:05 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,7 +26,7 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch() {
-
+	cardId = this.getAttribute("data-Id");
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
@@ -34,20 +34,32 @@ function checkForMatch() {
 }
 };
 
-function flipCard(cardId) {
-
+function flipCard() {
+	cardId = this.getAttribute("data-Id");
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push([cardId].rank);
-
 	console.log(cards[cardId].cardImage);
-
 	console.log(cards[cardId].suit);
-
+	this.setAttribute("src", "cardImage")
 	if (cardsInPlay.length === 2) {
-
 	checkForMatch()
 
 }
-}
-flipCard(0)
-flipCard(2)
+};
+
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id",i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+
+
+createBoard();
+
+// please note, towards the end of this project I used a lot of help from peoples screenshots from slack to try and get a fuller understanding. All of this is not totally my own work.
